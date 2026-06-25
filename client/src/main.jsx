49997b15bb49a6541e3a8e3042fd7bb81de77023
Nishkarsh <nishdixit0207@gmail.com>
2026-06-25 15:47:09 +0530

    blogs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import './index.css'
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import store from './redux/store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <HelmetProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </HelmetProvider>
 );
