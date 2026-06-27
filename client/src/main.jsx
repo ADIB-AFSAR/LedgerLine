f9326c01b8c8b6f0e2e2769b07d1b2f2214a131b
Adib Afsar <afsaradib786@gmail.com>
2026-06-27 13:30:16 +0530

    add: web analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store/store';
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
