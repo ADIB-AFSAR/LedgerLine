c25dc0e7d5d4a7974151c4193943d54b3c87647f
Adib Afsar <afsaradib786@gmail.com>
2026-06-27 17:53:58 +0530

    fix:analytics import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import store from './redux/store/store';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
